212c1626f46b70f0ea226bed1aab3a0cff716acd
kuninet <kuninet@gmail.com>
2015-12-29 23:19:21 +0900

    bug #1 BOX取得シェルスクリプトのバグ修正

--- ubuntu12_04/ubuntu_box_get.sh
+++ ubuntu12_04/ubuntu_box_get.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-vagrant box add ubuntu32 https://cloud-images.ubuntu.com/vagrant/precise/current/precise-server-cloudimg-amd64-vagrant-disk1.box
+vagrant box add ubuntu64 https://cloud-images.ubuntu.com/vagrant/precise/current/precise-server-cloudimg-amd64-vagrant-disk1.box
