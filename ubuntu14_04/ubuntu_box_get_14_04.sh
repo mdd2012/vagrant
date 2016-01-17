94fe1fc3c40f40bda79bce64031646407f8b05e8
kuninet <kuninet@gmail.com>
2016-01-17 10:33:00 +0900

    fix #1 box get shell script bug fix

--- ubuntu14_04/ubuntu_box_get_14_04.sh
+++ ubuntu14_04/ubuntu_box_get_14_04.sh
@@ -1,2 +1,1 @@
-#!/bin/sh
-vagrant box add ubuntu14_04_64 https://cloud-images.ubuntu.com/vagrant/precise/current/precise-server-cloudimg-amd64-vagrant-disk1.box
+vagrant box add ubuntu14_04_64 https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box
